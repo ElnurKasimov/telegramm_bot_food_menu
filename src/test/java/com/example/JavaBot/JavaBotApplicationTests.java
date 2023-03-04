c5fd3a78855e7de79054ff0874244a602d42d4ae
ElnurKasimov <ekasimov2012@gmail.com>
2023-03-04 22:33:55 +0200

    init project

---
+++ src/test/java/com/example/JavaBot/JavaBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JavaBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
