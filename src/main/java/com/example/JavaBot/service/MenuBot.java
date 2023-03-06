db9995da5244ff97026fa0b09abff8a3c9014065
ElnurKasimov <ekasimov2012@gmail.com>
2023-03-06 17:46:38 +0200

    added keyboard menu

--- src/main/java/com/example/JavaBot/service/MenuBot.java
+++ src/main/java/com/example/JavaBot/service/MenuBot.java
@@ -5,7 +5,12 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class MenuBot extends TelegramLongPollingBot {
@@ -26,14 +31,23 @@ public class MenuBot extends TelegramLongPollingBot {
         SendMessage sendMessage = new SendMessage();
         sendMessage.setText("Hello user! I've received Your message " + "\"" + message.getText() + "\"");
         sendMessage.setChatId(String.valueOf(message.getChatId()));
-        if ("breakfast".equals(message.getText())) {
+
+
+        if ("/start".equals(message.getText())) {
+            sendMessage.enableMarkdown(true);
+            ReplyKeyboardMarkup replyKeyboardMarkup = getMenuKeyboard();
+            sendMessage.setReplyMarkup(replyKeyboardMarkup);
+            sendMessage.setText("Welcome to Receipt Bot! Please chose meal of the day:\n");
+        }
+
+        if ("Breakfast".equals(message.getText())) {
             String menu = "Breakfast menu:\n";
             menu+="1. Banana-Nut Smothie\n";
             menu+="2. Omlet + Greens\n";
             menu+="3. Avocado Crispy Eggs Toast";
             sendMessage.setText(menu);
         }
-        if ("dinner".equals(message.getText())) {
+        if ("Dinner".equals(message.getText())) {
             String menu = "Dinner menu:\n";
             menu+="1. Creamy Lemon Chgiken Pasta\n";
             menu+="2. Turkey Tacos\n";
@@ -47,4 +61,29 @@ public class MenuBot extends TelegramLongPollingBot {
             e.printStackTrace();
         }
     }
+
+    private ReplyKeyboardMarkup getMenuKeyboard() {
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(false);
+
+        List<KeyboardRow> keyboardRows = new ArrayList<>();
+        KeyboardRow keyboardRow = new KeyboardRow();
+        keyboardRow.add("Breakfast");
+        keyboardRow.add("Dinner");
+        KeyboardRow keyboardSecondRow = new KeyboardRow();
+        keyboardSecondRow.add("Lunch");
+        keyboardSecondRow.add("Supper");
+
+        keyboardRows.add(keyboardRow);
+        keyboardRows.add(keyboardSecondRow);
+
+        replyKeyboardMarkup.setKeyboard(keyboardRows);
+
+        return replyKeyboardMarkup;
+
+    }
+
+
 }
